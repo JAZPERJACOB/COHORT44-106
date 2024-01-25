bb97a592c91c4ce4a556e5d988a9b2b12705e7bf
JAZPERJACOB <151716533+JAZPERJACOB@users.noreply.github.com>
2024-01-24 20:31:37 -0800

    Add files via upload

--- COHORT44-106/scripts/app.js
+++ COHORT44-106/scripts/app.js
@@ -1,53 +1,61 @@
-function sayHello(name, lastName){
-    console.log("Hello" + name + lastName);
+//global variables
+let iconImportant = false;
+
+function saveTask() {
+    console.log("Saving task");
+    //get the values
+    const title = $("#title").val();
+    const description = $("#description").val();
+    const color = $("#color").val();
+    const startDate = $("#startDate").val();
+    const status = $("#status").val();
+    const budget = $("#budget").val();
+    //build the object
+    let taskToSave = new Task(title, description, color, startDate, status, budget)
+    console.log(taskToSave);
+    //save logic
+    $.ajax({
+        type: "POST",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+        data: JSON.stringify(taskToSave),
+        contentType: "application/json",
+        success: function(res){
+        console.log(res);
+        },
+        error: function(error) {
+        console.log(error);
+        }
+    })
 }
 
-function sum(num1, num2){
-    const res=num1+num2;
-    return res;
-}
 
-function printNumber(){
-    //print numbers from 1 to 20
-    //except 7 and 13
-    let sum = 0;
-    let count = 0;
-    let i = 0;
-//print everynumber in the list
-let numbers = [12,4,123,4567,234,56,12,87,124,865,233,788,43,91,544,782,653,845];
-for (let i = 1; i<21; i++){
-    if (i !=7 && i != 13){
-        console.log(i);
+function changeIcon() {
+    console.log("Changing icon");
+    const nonImportant = "fa-solid fa-snowflake";
+    const isImportant = "fa-solid fa-mug-hot";
+    if (iconImportant) {
+        $("#iImportant").removeClass(isImportant).addClass(nonImportant);
+        iconImportant = false;
     }
-}
-for (let i = 0; i<numbers.length; i++) {
-    let num = numbers[i];
-    console.log(num);
-    //sum numbers
-    sum += num;
-    //only greater numbers
-    if (num > 500)
-    count += 1;
-}
-console.log("the sum of the numbers is; " + sum);
-console.log("there are" +count+ "number bigger than 500")
-//print the sum of all numbers
-//print how many numbers are greater than 500
+    else {
+        $("#iImportant").removeClass(nonImportant).addClass(isImportant);
+        iconImportant = true;
+    }
+    //please try to restore the Icon
 }
 
-function init(){
-    console.log("Hello World");
-    sayHello("Jazper", "Jacob");
-    const result = sum(21,12)
-    console.log(result);
+function displayTask(task){
+    let syntax = ``
 }
-//creat a function that sums to numbers together and execute it in the init
 
+function init() {
+    console.log("This is the parent of everything");
+    //load data
+
+    //hook events
+    $("#btnSave").click(saveTask);
+    $("#iImportant").click(changeIcon);
+    //document.getElementById("btnSave").click(saveTask)
+}
 
 window.onload = init;
-//window.onload = init(); this is a not
-
-//PRINCIPLE
-//DRY -- DONT REPEAT YOURSELF
-//KISS - KEEP IT SIMPLE STUPID
-//
